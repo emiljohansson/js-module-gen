61e98a0ec93a7ae32ac1a166532e5e6092ac857b
Emil Johansson <emiljohansson.se@gmail.com>
2014-08-29 14:47:26 -0500

    add simple class module support

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 'use strict';
 var fs = require('fs');
 
-var args = process.argv.slice(2),
-    dir = args.length >= 1 ? args[0] : "",
-    fullPath = process.cwd()+'/'+dir+'/moduleName.js';
+var args = process.argv.slice(2);
+var dir = (function() {
+        if (args.length < 1) return "";
+        if (args[0].indexOf('--') < 0) return args[0];
+        return "";
+    }());
+var fullPath = process.cwd()+'/'+dir+'/moduleName.js';
 
-fs.readFile(__dirname+'/template.js', 'utf8', function(err, data) {
+function getTemplate() {
+    var tmpl = 'tmpl_module.js';
+    args.forEach(function(argument) {
+        if (argument === '--simple' || argument === '--s' || argument === '--class') {
+            tmpl = 'tmpl_class.js';
+            return false;
+        }
+    });
+    return tmpl;
+}
+
+fs.readFile(__dirname+'/'+getTemplate(), 'utf8', function(err, data) {
     if (err) return console.log(err);
     generate(data);
 });
---
+++ tmpl_class.js
@@ -0,0 +1,6 @@
+(function() {
+    'use strict';
+    function ModuleName() {}
+
+    window.ModuleName = ModuleName;
+}());
